b4c0b82231129cfcf3ec34c26fcffaa5f7ccb4ef
Nixomart <80935886+Nixomart@users.noreply.github.com>
2022-04-02 11:57:47 -0300

    calculadora terminada funcionando

--- src/components/boton.js
+++ src/components/boton.js
@@ -5,10 +5,11 @@ import '../styles/boton.css'
 export default function boton(props) {
 
     const isOperator = valor => {
-        return isNaN(valor) && (valor != '.') && (valor != '=');
+        return isNaN(valor) && (valor !== '.') && (valor !== '=');
     }
     return (
-        <div className={`button-container ${isOperator(props.children) ? 'operator' : ''}`.trimEnd()}>
+        /** EN ONCLICK () => ES PARA QUE EL DIV DIGA QUE SE TIENE QUE LLAMAR ALA FUNCION */
+        <div onClick={() => props.manipuleClick(props.children)} className={`button-container ${isOperator(props.children) ? 'operator' : ''}`.trimEnd()}>
             {props.children}
         </div>
     )
